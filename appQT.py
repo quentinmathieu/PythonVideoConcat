5d374bfc575b69e8a601519e04724fd7ccef4f7e
quentinmathieu <quentin.mth@gmail.com>
2024-03-24 23:00:04 +0100

    QThread managed

--- appQT.py
+++ appQT.py
@@ -1,12 +1,34 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6 import uic
 from PyQt6.QtGui import *
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
 import os, sys, pyperclip, json,html
 from datetime import datetime
 import ffmpeg
-import threading
-import multiprocessing
+
+class ConcatenateThread(QThread):
+        finished = pyqtSignal(str)
+        error = pyqtSignal(str)
+
+        def __init__(self, concat_file_path, output_file, myGui):
+            super().__init__()
+            self.concat_file_path = concat_file_path
+            self.output_file = output_file
+            self.myGui = myGui
+
+        def run(self):
+            try:
+                ffmpeg.input(self.concat_file_path, format='concat', safe=0).output(self.output_file, c='copy').run()
+                self.myGui.videoInfos.setText("Videos concatenated and saved as \n"+self.output_file.replace('\\',"/"))
+                # output_message = "Concatenation completed successfully."
+                print(self.concat_file_path)
+                # Clean up temporary files
+                os.remove(self.concat_file_path)
+                self.myGui.setStatusInterface(True)
+                print("Concatenation complete!")
+                self.finished.emit("ok")
+            except Exception as e:
+                self.error.emit(str(e))
 
 
 class MyGUI(QMainWindow):
@@ -25,10 +47,68 @@ class MyGUI(QMainWindow):
         self.compressBtn.clicked.connect(lambda: self.crompressVideos())
         self.delListBtn.clicked.connect(lambda: self.deleteFromList())
 
+
+
+
+    
+
     def on_click(self):
-        m = multiprocessing.Process(target=self.concatenate_videos())
-        m.start()
-        m.join()
+        self.videoInfos.setText("Prosessing...")
+        file_paths = [self.filesList.item(x).text() for x in range(self.filesList.count())]
+        self.setStatusInterface(False)
+        
+        if len(file_paths) < 2:
+            self.setStatusInterface(True)
+            self.videoInfos.setText("Select at least 2 videos to concatenate.")
+            return
+
+        codec = None
+        for file_path in file_paths:
+            # Use ffmpeg to get codec information of the videos
+            try:
+                probe = ffmpeg.probe(file_path)
+            except Exception as error:
+                self.setStatusInterface(True)
+                self.videoInfos.setText("An exception occurred:", error)
+                return 
+            video_info = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
+            if codec is None:
+                codec = video_info['codec_name']
+            elif codec != video_info['codec_name']:
+                self.setStatusInterface(True)
+                self.videoInfos.setText("Videos have different codecs.")
+                return
+
+        # Filter videos by codec
+        filtered_files = [f for f in file_paths if ffmpeg.probe(f)['streams'][0]['codec_name'] == codec]
+
+        if len(filtered_files) < 2:
+            self.videoInfos.setText("No videos with the same codec found.")
+            return
+
+
+        # Get the directory of the last file for the ouput path
+        last_file_dir = os.path.dirname(file_paths[-1])
+
+        # Generate the concat demuxer file
+        concat_content = [f"file '{file_path}'" for file_path in filtered_files]
+        concat_file_path =     output_file = os.path.join(last_file_dir, "concat.txt") 
+
+        with open(concat_file_path, "w") as concat_file:
+            concat_file.write('\n'.join(concat_content))
+
+
+        # Get current timestamp to create output filename to the same directory as the last input file
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        output_file = os.path.join(last_file_dir, f"output_{current_time}.mp4") 
+
+        self.concatThread = ConcatenateThread(concat_file_path, output_file, self)
+        # self.concatThread.finished.connect(self.concatenationFinished)
+        # self.concatThread.error.connect(self.concatenationError)
+        self.concatThread.start()
+
+       
+        
 
 
     def dragEnterEvent(self, event):
@@ -69,65 +149,6 @@ class MyGUI(QMainWindow):
     def crompressVideos(self):
         self.videoInfos.setText("WIP")
 
-    def concatenate_videos(self):
-        self.videoInfos.setText("Prosessing...")
-        file_paths = [self.filesList.item(x).text() for x in range(self.filesList.count())]
-        self.setStatusInterface(False)
-        
-        if len(file_paths) < 2:
-            self.setStatusInterface(True)
-            self.videoInfos.setText("Select at least 2 videos to concatenate.")
-            return
-    
-        codec = None
-        for file_path in file_paths:
-            # Use ffmpeg to get codec information of the videos
-            try:
-                probe = ffmpeg.probe(file_path)
-            except Exception as error:
-                self.setStatusInterface(True)
-                self.videoInfos.setText("An exception occurred:", error)
-                return 
-            video_info = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
-            if codec is None:
-                codec = video_info['codec_name']
-            elif codec != video_info['codec_name']:
-                self.setStatusInterface(True)
-                self.videoInfos.setText("Videos have different codecs.")
-                return
-    
-        # Filter videos by codec
-        filtered_files = [f for f in file_paths if ffmpeg.probe(f)['streams'][0]['codec_name'] == codec]
-    
-        if len(filtered_files) < 2:
-            self.videoInfos.setText("No videos with the same codec found.")
-            return
-    
-
-        # Get the directory of the last file for the ouput path
-        last_file_dir = os.path.dirname(file_paths[-1])
-
-        # Generate the concat demuxer file
-        concat_content = [f"file '{file_path}'" for file_path in filtered_files]
-        concat_file_path =     output_file = os.path.join(last_file_dir, "concat.txt") 
-
-        with open(concat_file_path, "w") as concat_file:
-            concat_file.write('\n'.join(concat_content))
-    
-
-        # Get current timestamp to create output filename to the same directory as the last input file
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        output_file = os.path.join(last_file_dir, f"output_{current_time}.mp4") 
-        # Concatenate videos using ffmpeg
-        try:
-            ffmpeg.input(concat_file_path, format='concat', safe=0).output(output_file, c='copy').run()
-            self.videoInfos.setText("Videos concatenated and saved as \n"+output_file.replace('\\',"/"))
-        except ffmpeg.Error as e:
-            self.videoInfos.setText("Error concatenating videos: "+str(e))
-        self.setStatusInterface(True)
-        # Clean up temporary files
-        os.remove(concat_file_path)
-        print("Concatenation complete!")
 
 def main():
     try :
