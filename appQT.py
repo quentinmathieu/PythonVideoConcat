d86543f773f6d84e9d4d03922f4eb3dc6f1eae65
quentinmathieu <quentin.mth@gmail.com>
2024-03-27 19:23:45 +0100

    add compress feature

--- appQT.py
+++ appQT.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QSlider
 from PyQt6 import uic
 from PyQt6.QtGui import *
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
@@ -11,16 +11,22 @@ class ConcatenateThread(QThread):
         finished = pyqtSignal(str)
         error = pyqtSignal(str)
 
-        def __init__(self, concat_file_path, output_file, myGui):
+        def __init__(self, concat_file_path, output_file, myGui, crf = 0):
             super().__init__()
             self.concat_file_path = concat_file_path
             self.output_file = output_file
             self.myGui = myGui
+            self.crf = crf
 
         def run(self):
             try:
-                ffmpeg.input(self.concat_file_path, format='concat', safe=0).output(self.output_file, c='copy').run()
-                self.myGui.videoInfos.setText("Videos concatenated and saved as \n"+self.output_file.replace('\\',"/"))
+                if self.crf>0:
+                    ffmpeg.input(self.concat_file_path, format='concat', safe=0).output(self.output_file, vcodec='libx264', crf=self.crf).run()
+                    self.myGui.videoInfos.setText("Videos concatenated and saved as \n"+self.output_file.replace('\\',"/"))
+                else:
+                    ffmpeg.input(self.concat_file_path, format='concat', safe=0).output(self.output_file, c='copy').run()
+                    self.myGui.videoInfos.setText("Videos concatenated and saved as \n"+self.output_file.replace('\\',"/"))
+                    
 
                 # Clean up temporary files
                 os.remove(self.concat_file_path)
@@ -46,6 +52,7 @@ class MyGUI(QMainWindow):
         self.concatBtn.clicked.connect(lambda: self.on_click())
         self.compressBtn.clicked.connect(lambda: self.crompressVideos())
         self.delListBtn.clicked.connect(lambda: self.deleteFromList())
+
         
         
 
@@ -81,6 +88,7 @@ class MyGUI(QMainWindow):
         filtered_files = [f for f in file_paths if ffmpeg.probe(f)['streams'][0]['codec_name'] == codec]
 
         if len(filtered_files) < 2:
+            self.setStatusInterface(True)
             self.videoInfos.setText("No videos with the same codec found.")
             return
 
@@ -101,10 +109,11 @@ class MyGUI(QMainWindow):
         output_file = os.path.join(last_file_dir, f"output_{current_time}.mp4") 
 
         self.concatThread = ConcatenateThread(concat_file_path, output_file, self)
-        self.stopBtn.clicked.connect(lambda: self.stop(output_file, concat_file_path))
+        self.stopBtn.clicked.connect(lambda: self.stop())
         self.concatThread.start()
 
-    def stop(self, output_file, concat_file_path):
+    def stop(self):
+        #stop concat / compress
         PROCNAME = "ffmpeg.exe"
 
         
@@ -140,7 +149,10 @@ class MyGUI(QMainWindow):
             self.filesList.takeItem(self.filesList.row(item))
 
 
+
+
     def setStatusInterface(self, status):
+        #reset interface
         invers = False if status else True
         self.clearBtn.setEnabled(status)
         self.concatBtn.setEnabled(status)
@@ -154,14 +166,40 @@ class MyGUI(QMainWindow):
         self.setAcceptDrops(status)
 
     def crompressVideos(self):
-        self.videoInfos.setText("WIP")
+        self.videoInfos.setText("Prosessing...")
+        file_paths = [self.filesList.item(x).text() for x in range(self.filesList.count())]
+        self.setStatusInterface(False)
+
+        if len(file_paths) < 1:
+            self.setStatusInterface(True)
+            self.videoInfos.setText("Select at least 1 video to convert.")
+            return
+
+        # Filter videos by codec
+        filtered_files = [f for f in file_paths]
+
+        # Get the directory of the last file for the ouput path
+        last_file_dir = os.path.dirname(file_paths[-1])
+
+        # Generate the concat demuxer file
+        concat_content = [f"file '{file_path}'" for file_path in filtered_files]
+        concat_file_path = os.path.join(last_file_dir, "concat.txt") 
+        
+        with open(concat_file_path, "w") as concat_file:
+            concat_file.write('\n'.join(concat_content))
+        
+        # Get current timestamp to create output filename to the same directory as the last input file
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        output_file = os.path.join(last_file_dir, f"output_{current_time}_compress.mp4") 
+        self.concatThread = ConcatenateThread(concat_file_path, output_file, self, self.compressSlider.value())
+        self.stopBtn.clicked.connect(lambda: self.stop())
+        self.concatThread.start()
 
 
 def main():
     try :
         # Add ffmpeg to the PATH
         ffmpegPath= os.path.dirname(os.path.realpath(__file__))+"\\ffmpeg\\bin"
-        print(ffmpegPath)
         os.environ['PATH'] = ffmpegPath
     except Exception as error:
         print("An exception occurred:"+ str(error))
